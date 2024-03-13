0caca798d9fe0203ab9fb1e9e55e4a278b4ba1c6
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 02:25:59 -0700

    Update Login.js
    login form

--- front_end/src/Pages/Login.js
+++ front_end/src/Pages/Login.js
@@ -1,12 +1,64 @@
-import React from 'react'
 
+import { Link, useNavigate } from "react-router-dom"
+import { useRef } from "react"
+import axios from "../axiosConfig"
+// import { useJwt } from "react-jwt"
+import classes from "./login.module.css"
 function Login() {
-  return (
-    <div>
-      
-    </div>
-  )
+    // const token = useJwt()
+    const navigate = useNavigate()
+
+    const emailDom = useRef()
+    const passwordDom = useRef()
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const emailValue = emailDom.current.value;
+        const passwordValue = passwordDom.current.value
+        if (
+            !emailValue ||
+            !passwordValue
+        ) {
+            alert("please provide all")
+            return;
+        }
+        try {
+            const { data } = await axios.post("http://localhost:4040/api/users/login", {
+                email: emailValue,
+                password: passwordValue,
+            });
+            alert("login success")
+            localStorage.setItem("token", data.token);
+            navigate("/")
+            console.log(data)
+        } catch (error) {
+            alert("Something wrong")
+            console.log(error.response)
+        }
+    }
+
+    return (
+        <section className={classes.login}>
+            <div className={classes.login_container}>
+                <form onSubmit={handleSubmit}>
+                    <h1>Sign In</h1>
+                <div>
+                    <span>email</span>
+                    <input ref={emailDom} type="email" placeholder="email" />
+                </div>
+                
+                <div>
+                    <span>password</span>
+                    <input ref={passwordDom} type="password" placeholder="password" />
+                </div>
+            
+                <button type="submit" className={classes.btn_dark} id="newFormBtn">Login</button>
+                </form>
+                <p>Well come! our pages.If you have not an account please create your account</p>
+                <button className={classes.register}> <Link to={"/register"} >register </Link></button>
+            </div>
+        </section>
+    )
 }
 
 export default Login
-
