0ccdd7c97d14034794de5ac65508f10875b17934
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 04:59:07 -0700

    Create ipad.js

---
+++ front_end/src/Component/ipad.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Ipad(){
+    return(
+        <div>
+<section className="internal_wraper">
+<div className="container">
+<div className="row">
+    <div className="col-12 mt-5 pt-5">
+<h1 className="font-weight-bold">
+ipad
+</h1>
+    </div>
+</div>
+</div>
+</section>
+        </div>
+    );
+};
+
+export default Ipad;
