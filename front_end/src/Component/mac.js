85d252b0ae3317dcd244a794f6bd7cc34503cfa5
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 04:59:59 -0700

    Create mac.js

---
+++ front_end/src/Component/mac.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Mac(){
+    return(
+        <div>
+<section className="internal_wraper">
+<div className="container">
+<div className="row">
+    <div className="col-12 mt-5 pt-5">
+<h1 className="font-weight-bold">
+Mac phone
+</h1>
+    </div>
+</div>
+</div>
+</section>
+        </div>
+    );
+};
+
+export default Mac;
