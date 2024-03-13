ae8d1467f125f3578beb27bc4f266c4fe44a4d50
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 05:00:14 -0700

    Create Suport.js

---
+++ front_end/src/Component/Suport.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Suport(){
+    return(
+        <div>
+<section className="internal_wraper">
+<div className="container">
+<div className="row">
+    <div className="col-12 mt-5 pt-5">
+<h1 className="font-weight-bold">
+Suport
+</h1>
+    </div>
+</div>
+</div>
+</section>
+        </div>
+    );
+};
+
+export default Suport;
