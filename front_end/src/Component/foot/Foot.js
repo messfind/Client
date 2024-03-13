eb8e216f425d497a66da3afff763018fa78ca737
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 04:57:20 -0700

    Create Foot.js

---
+++ front_end/src/Component/foot/Foot.js
@@ -0,0 +1,159 @@
+import React from 'react';
+
+function Foot(){
+    return(
+        <div>
+<footer className="footer-wrapper">
+            <div className="uper-text">
+                1. Trade-in values will vary based on the condition, year, and configuration of your eligible trade-in
+                device. Not all
+                devices are eligible for credit. You must be at least 18 years old to be eligible to trade in for credit
+                or for an Apple
+                Gift Card. Trade-in value may be applied toward qualifying new device purchase, or added to an Apple
+                Gift Card. Actual
+                value awarded is based on receipt of a qualifying device matching the description provided when estimate
+                was made. Sales
+                tax may be assessed on full value of a new device purchase. In-store trade-in requires presentation of a
+                valid photo ID
+                (local law may require saving this information). Offer may not be available in all stores, and may vary
+                between in-store
+                and online trade-in. Some stores may have additional requirements. Apple or its trade-in partners
+                reserve the right to
+                refuse or limit quantity of any trade-in transaction for any reason. More details are available from
+                Apple’s trade-in
+                partner for trade-in and recycling of eligible devices. Restrictions and limitations may apply.
+
+                Apple Vision Pro will be available early next year on <a href="#">apple.com</a> and at Apple retail
+                stores in the U.S.
+            </div>
+            <div className="footer-link-wrapper">
+                <div className="link-wrapper-1">
+                    <h3>Shop and learn</h3>
+                    <ul>
+                        <li><a href="#">Store</a></li>
+                        <li><a href="#">Mac</a></li>
+                        <li><a href="#">iPad</a></li>
+                        <li><a href="#">iphon</a></li>
+                        <li><a href="#">Watch</a></li>
+                        <li><a href="#">Vision</a></li>
+                        <li><a href="#">Airpods</a></li>
+                        <li><a href="#">TV & Home</a></li>
+                        <li><a href="#">Air tag</a></li>
+                        <li><a href="#">Accessories</a></li>
+                        <li><a href="#">Gift card</a></li>
+                        <div className="link-wrapper-2">
+                            <h3>Apple Wallet</h3>
+                            <li><a href="#">Wallet</a></li>
+                            <li><a href="#">Apple card</a></li>
+                            <li><a href="#">Apple pay</a></li>
+                            <li><a href="#">Apple cash</a></li>
+                        </div>
+                    </ul>
+                </div>
+                <div className="link-wrapper-3">
+                    <h3>Account</h3>
+                    <ul>
+                        <li><a href="#">Manage Your Apple ID</a></li>
+                        <li><a href="#">Apple Store Account</a></li>
+                        <li><a href="#">iCloud.com</a></li>
+                        <div className="link-wrapper-4">
+                            <h3>Entertainment</h3>
+                            <li><a href="#">Apple One</a></li>
+                            <li><a href="#">Apple TV+</a></li>
+                            <li><a href="#">Apple Music</a></li>
+                            <li><a href="#">Apple arcade</a></li>
+                            <li><a href="#">Apple Fitness+</a></li>
+                            <li><a href="#">Apple News+</a></li>
+                            <li><a href="#">Apple Podcasts</a></li>
+                            <li><a href="#">Apple Books</a></li>
+                            <li><a href="#">App Store</a></li>
+                        </div>
+                    </ul>
+                </div>
+                <div className="link-wrapper-5">
+                    <h3>Apple Store</h3>
+                    <ul>
+                        <li><a href="#">Find a Store</a></li>
+                        <li><a href="#">Genius Bar</a></li>
+                        <li><a href="#">Today at Apple</a></li>
+                        <li><a href="#">Apple Camp</a></li>
+                        <li><a href="#">Apple Store App</a></li>
+                        <li><a href="#">Certified Refurbished</a></li>
+                        <li><a href="#">Apple Trade In</a></li>
+                        <li><a href="#">Financing</a></li>
+                        <li><a href="#">Carrier Deals at Apple</a></li>
+                        <li><a href="#">Order Status</a></li>
+                        <li><a href="#">Shopping Help</a></li>
+                    </ul>
+                </div>
+                <div className="link-wrapper-6">
+                    <h3>For business</h3>
+                    <ul>
+                        <li><a href="#">Apple and Business</a></li>
+                        <li><a href="#">Shop for Business</a></li>
+                        <div className="link-wrapper-7">
+                            <h3>For education</h3>
+                            <li><a href="#">Apple and Education</a></li>
+                            <li><a href="#">Shop for K-12</a></li>
+                            <li><a href="#">Shop for College</a></li>
+                        </div>
+                        <div className="link-wrapper-8">
+                            <h3>For Healthcare</h3>
+                            <li><a href="#">Apple in Healthcare</a></li>
+                            <li><a href="#">Health on Apple Watch</a></li>
+                            <li><a href="#">Health Records on iPhone</a></li>
+                        </div>
+                        <div className="link-wrapper-9">
+                            <h3>For Government</h3>
+                            <li><a href="#">Shop for Government</a></li>
+                            <li><a href="#">Shop for Veterans and Military</a></li>
+                        </div>
+                    </ul>
+                </div>
+                <div className="link-wrapper-10">
+                    <h3>Apple Values</h3>
+                    <ul>
+                        <li><a href="#">Accessibility</a></li>
+                        <li><a href="#">Education</a></li>
+                        <li><a href="#">Environment</a></li>
+                        <li><a href="#">Inclusion and Diversity</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Racial Equity and Justice</a></li>
+                        <li><a href="#">Supplier Responsibility</a></li>
+                        <div className="link-wrapper-11">
+                            <h3>About Apple</h3>
+                            <li><a href="#">Newsroom</a></li>
+                            <li><a href="#">Apple Leadership</a></li>
+                            <li><a href="#">Career Opportunities</a></li>
+                            <li><a href="#">Investors</a></li>
+                            <li><a href="#">Ethics & Compliance</a></li>
+                            <li><a href="#">Events</a></li>
+                            <li><a href="#">Contact Apple</a></li>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+        <section className="end">
+            <div className="foot">
+                <p>ways to shop: <a href="#">Find an Apple Store</a> or <a href="#">other retailer</a> near you. Or
+                    call 1-800-MY-APPLE.</p>
+            </div>
+            <hr />
+            <div className="last">
+                <ul>
+                    <li className="one">Copyright © 2023 Apple Inc. All rights reserved.</li>
+                    <li className="two"><a href="#"> Privacy policy</a></li>
+                    <li className="three"><a href="#"> Terms of use</a></li>
+                    <li className="four"><a href="#"> Sells and refund</a></li>
+                    <li className="five"><a href="#"> Legal</a></li>
+                    <li className="six"><a href="#"> Site map</a></li>
+                    <li className="seven"><a href="#">United states</a></li>
+                </ul>
+            </div>
+        </section> 
+        </div>
+    )
+}
+
+export default Foot;
