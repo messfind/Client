f8e9138d4f0cc2ca5f8e5d159875f08f5f89a4cd
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 04:57:17 -0700

    Create Entertainment.js

---
+++ front_end/src/Component/Entertainment.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Entertainment(){
+    return(
+        <div>
+<section className="internal_wraper">
+<div className="container">
+<div className="row">
+    <div className="col-12 mt-5 pt-5">
+<h1 className="font-weight-bold">
+Entertainment
+</h1>
+    </div>
+</div>
+</div>
+</section>
+        </div>
+    );
+};
+
+export default Entertainment;
