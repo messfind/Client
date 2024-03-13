3a71dba54930d7d4c383399ad0f6fd4f010bef8e
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 04:59:57 -0700

    Create iphon.js

---
+++ front_end/src/Component/iphon.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './iphon.css'
+function Iphon(){
+    return(
+        <div>
+<section className="internal_wraper">
+<div className="container">
+<div className="row">
+    <div className="col-12 mt-5 pt-5">
+<h1 className="font-weight-bold">
+iphon
+</h1>
+    </div>
+</div>
+</div>
+</section>
+        </div>
+    );
+};
+
+export default Iphon;
