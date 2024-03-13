81fa5607f2727655fadea520b7a2ab05f5856d15
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 04:55:50 -0700

    Create tv.js

---
+++ front_end/src/Component/tv.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Tv(){
+    return(
+        <div>
+<section className="internal_wraper">
+<div className="container">
+<div className="row">
+    <div className="col-12 mt-5 pt-5">
+<h1 className="font-weight-bold">
+Tv
+</h1>
+    </div>
+</div>
+</div>
+</section>
+        </div>
+    );
+};
+
+export default Tv;
