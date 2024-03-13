a0bf266a3aee5af13a8b2d17577ad720cf0a6d40
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 04:56:43 -0700

    Create Four404.js
    when page missing url

---
+++ front_end/src/Component/Four404.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Four404(){
+    return(
+        <div>
+<section className="internal_wraper">
+<div className="container">
+<div className="row">
+    <div className="col-12 mt-5 pt-5">
+<h1 className="font-weight-bold">
+404
+page not found!
+</h1>
+    </div>
+</div>
+</div>
+</section>
+        </div>
+    );
+};
+
+export default Four404;
