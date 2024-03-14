45c69f04561848d621eee3a2dbb1782f94275aea
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-13 22:45:52 -0700

    Update app.js
    to install and import node modules

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,57 @@
-const mySql = require("mysql2")
+require("dotenv").config()
+const express = require("express")
+const app = express();
 
-var mysqlConnection = mySql.createConnection({
-    host: "localhost",
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    connectionLimit: 10
+const cors = require("cors")
+app.use(cors())
+
+app.use(express.json());
+
+
+app.use(express.urlencoded({
+    extended: true,
+}))
+//db connection
+const mysqlConnection = require("./dbconfig")
+
+//user route middleware file
+const userRoute = require("./route/userRoute")
+
+const questionRoutes = require("./route/question");
+
+//authentication middleware
+const authMiddleware = require("./middleware/authMiddleware");
+
+//json middleware to extract json data
+app.use(express.json())
+
+//user route middleware
+app.use("/api/users", userRoute)
+
+
+
+//questions route middleware
+app.use("/api/questions", authMiddleware, questionRoutes)
+
+
+//answers route middleware
+
+async function start() {
+    try {
+        const result = await mysqlConnection.execute("select'test'")
+        console.log(result)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+start()
+
+
+const port = 4040
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err.message);
+    } else { console.log(`listen on ${port}`) }
+
 });
-
-module.exports = mysqlConnection.promise()
